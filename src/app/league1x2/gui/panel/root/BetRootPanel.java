d1d59a5422ee2f80ae369370bebf337b164bfa48
Aemilus <35335174+Aemilus@users.noreply.github.com>
2024-04-02 00:37:05 +0300

    league 1x2 - use linkedhashmap for pairing bet selection and odd

---
+++ src/app/league1x2/gui/panel/root/BetRootPanel.java
@@ -0,0 +1,23 @@
+
+package app.league1x2.gui.panel.root;
+
+import app.league1x2.gui.panel.control.BetsControlPanel;
+import app.league1x2.gui.panel.input.BetInputPanel;
+import app.league1x2.gui.panel.view.BetsViewPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BetRootPanel extends JPanel {
+    public final BetInputPanel betInputPanel = new BetInputPanel();
+    public final BetsViewPanel betsViewPanel = new BetsViewPanel();
+    public final BetsControlPanel betsControlPanel = new BetsControlPanel();
+
+    public BetRootPanel() {
+        setLayout(new BorderLayout(5, 5));
+        add(betInputPanel, BorderLayout.NORTH);
+        add(betsViewPanel, BorderLayout.CENTER);
+        add(betsControlPanel, BorderLayout.SOUTH);
+    }
+
+}
