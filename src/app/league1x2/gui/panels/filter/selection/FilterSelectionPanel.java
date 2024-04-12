188d623b92a8d86f7521e299b78bd3d1108e08e2
Aemilus <35335174+Aemilus@users.noreply.github.com>
2024-04-12 05:23:53 +0300

    league 1x2 - refactor filter selection panel

--- src/app/league1x2/gui/panels/filter/selection/FilterSelectionPanel.java
+++ src/app/league1x2/gui/panels/filter/selection/FilterSelectionPanel.java
@@ -6,47 +6,28 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FilterSelectionPanel extends JPanel{
-    private JTextField selectionTextField;
-    private final JTextField minSelectionTextField = new JTextField(null, 5);
-    private final JTextField maxSelectionTextField = new JTextField(null, 5);
+    private final JTextField selectionTextField = new JTextField(null, 4);
+    private final SelectionRangePanel selectionRangePanel = new SelectionRangePanel();
 
     public FilterSelectionPanel(String selection) {
         initSelectionTextField(selection);
-        JPanel panel = initSelectionRangePanel();
+
         setLayout(new BorderLayout(15, 5));
         add(selectionTextField, BorderLayout.WEST);
-        add(panel, BorderLayout.CENTER);
+        add(selectionRangePanel, BorderLayout.CENTER);
     }
 
     private void initSelectionTextField(String selection) {
-        selectionTextField = new JTextField(selection, 4);
+        selectionTextField.setText(selection);
         selectionTextField.setMargin(new Insets(2, 4, 2, 4));
         selectionTextField.setHorizontalAlignment(SwingConstants.CENTER);
         selectionTextField.setEditable(false);
     }
 
-    private JPanel initSelectionRangePanel() {
-        minSelectionTextField.setMargin(new Insets(2, 4, 2, 4));
-        minSelectionTextField.setHorizontalAlignment(SwingConstants.CENTER);
-        maxSelectionTextField.setMargin(new Insets(2, 4, 2, 4));
-        maxSelectionTextField.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel separator = new JLabel("-");
-        separator.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout(5, 5));
-        panel.add(minSelectionTextField, BorderLayout.WEST);
-        panel.add(separator, BorderLayout.CENTER);
-        panel.add(maxSelectionTextField, BorderLayout.EAST);
-
-        return panel;
-    }
-
     public SelectionRange getSelectionRange() {
         String selection = selectionTextField.getText();
-        String min = minSelectionTextField.getText();
-        String max = maxSelectionTextField.getText();
+        String min = selectionRangePanel.minSelectionTextField.getText();
+        String max = selectionRangePanel.maxSelectionTextField.getText();
         return new SelectionRange(selection, min, max);
     }
 
---
+++ src/app/league1x2/gui/panels/filter/selection/SelectionRangePanel.java
@@ -0,0 +1,40 @@
+package app.league1x2.gui.panels.filter.selection;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SelectionRangePanel extends JPanel {
+    public final JTextField minSelectionTextField = new JTextField(null, 5);
+    private final JLabel separator = new JLabel("-");
+    public final JTextField maxSelectionTextField = new JTextField(null, 5);
+
+    public SelectionRangePanel() {
+        initComponents();
+
+        setLayout(new BorderLayout(5, 5));
+        add(minSelectionTextField, BorderLayout.WEST);
+        add(separator, BorderLayout.CENTER);
+        add(maxSelectionTextField, BorderLayout.EAST);
+    }
+
+    private void initComponents() {
+        initMinSelectionTextField();
+        initSeparator();
+        initMaxSelectionTextField();
+    }
+
+    private void initMinSelectionTextField() {
+        minSelectionTextField.setMargin(new Insets(2, 4, 2, 4));
+        minSelectionTextField.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void initSeparator() {
+        separator.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void initMaxSelectionTextField() {
+        maxSelectionTextField.setMargin(new Insets(2, 4, 2, 4));
+        maxSelectionTextField.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+}
